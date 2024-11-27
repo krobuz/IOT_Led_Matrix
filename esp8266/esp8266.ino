141cc6e4aa59d51f5b123f19020cdf4a59db0768
krobuz <minhbui12.2003@gmail.com>
2024-11-27 23:23:36 +0700

    fix sending data error

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -3,7 +3,8 @@
 
 const char* ssid = "Minh Nhi";
 const char* password = "tumotdentam";
-
+const char* ip_host = "192.168.1.78";
+const uint16_t port = 3000;   
 WebSocketsClient webSocket;
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
@@ -12,11 +13,14 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
             Serial.println("Disconnected");
             break;
         case WStype_CONNECTED:
-            Serial.println("Connected to WebSocket server");
+            Serial.println("WebSocket connected");
             webSocket.sendTXT("Hello from ESP8266");
             break;
         case WStype_TEXT:
             Serial.printf("Received: %s\n", payload);
+            break;
+        case WStype_ERROR:
+            Serial.printf("WebSocket Error: %s\n", payload);
             break;
     }
 }
@@ -32,7 +36,7 @@ void setup() {
     Serial.print("ESP8266 IP Address: ");
     Serial.println(WiFi.localIP()); 
 
-    webSocket.begin("192.168.1.64", 8080, "/");
+    webSocket.begin(ip_host, port);
     webSocket.onEvent(webSocketEvent);
 }
 
--- server.js
+++ server.js
@@ -7,45 +7,35 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-const esp8266Ws = new WebSocket('ws://192.168.1.65:8080');
 
-// Serve a basic webpage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Handle WebSocket connections
 wss.on('connection', (ws) => {
-    console.log('Web client connected'); // Log when the web client connects
+    console.log('Client connected');
 
-    // Handle incoming messages from the web client
     ws.on('message', (message) => {
-        console.log('Received from web client:', message);
+        const decodedMessage = message.toString();
+        console.log('Received from client:', decodedMessage);
 
-        // Check if ESP8266 WebSocket is connected
-        if (esp8266Ws.readyState === WebSocket.OPEN) {
-            // Send the received message to the ESP8266
-            esp8266Ws.send(message);
-            console.log('Message forwarded to ESP8266');
-        } else {
-            console.error('ESP8266 WebSocket is not connected');
-        }
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                console.log(`Forwarding message to client: ${decodedMessage}`);
+                client.send(decodedMessage);
+            }
+        })
+        
+    });
+    ws.on('close', () => {
+        console.log('Client disconnected');
     });
 
-    // Send a message to the client (optional)
     ws.send('Hello from Node.js Server');
 });
 
-esp8266Ws.on('open', () => {
-    console.log('Connected to ESP8266');
-});
 
-esp8266Ws.on('message', (message) => {
-    console.log('Message from ESP8266:', message.toString());
-});
-
-// Start the server
-const PORT = 8080;
+const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
